a076b20e6f9c8d4e018e37c8b3dc594229bbbbc8
playrey2010 <playrey2010@gmail.com>
2019-09-05 15:09:29 -0400

    incomplete

---
+++ src/Loops.java
@@ -0,0 +1,63 @@
+import java.util.Scanner;
+
+public class Loops {
+
+    public static void main(String[] args) {
+        // Write a program that prints the numbers 1-10.
+//        for (int i = 1; i < 11; i++) {
+//            System.out.println(i);
+//        }
+        // Write a program that prints the numbers 1-10 backwards.
+//        for (int i = 10; i > 0; i--){
+//            System.out.println(i);
+//        }
+
+        // Write a program that prints all the odd numbers from 1-20.
+        // I could have used modulus here; modulus on next example
+//        for (int i =1; i < 21; i+=2) {
+//            System.out.println(i);
+//        }
+
+        // Write a program that prints all the even numbers from 1-20.
+//        for (int i = 1; i < 21; i++) {
+//            if (i%2==0) {
+//                System.out.println(i);
+//            }
+//        }
+        // Have the user input a number and print all numbers from that input value to 1.
+//        Scanner sc = new Scanner(System.in);
+//        int num1;
+//
+//        System.out.print("Enter a number: ");
+//        num1 = sc.nextInt();
+//
+//        for (int i = num1; i >= 1 ; i-- ) {
+//            System.out.println(i);
+//        }
+        // Sum all values from 1-10.
+//        int sum = 0;
+//        for (int i = 1; i < 11; i++) {
+//            sum += i;
+//            System.out.println(sum);
+//        }
+        // Have the user enter a number and print the sum of all numbers from that inputted value to 1.
+//        Scanner sc = new Scanner(System.in);
+//        int num1;
+//        int sum = 0;
+//
+//        System.out.print("Enter a number: ");
+//        num1 = sc.nextInt();
+//
+//        for (int i = num1; i >= 1; i--) {
+//            sum += i;
+//            System.out.println("The sum right now is " + sum);
+//        }
+        for (int i = 0; i < 4; i++) {
+            System.out.println("**********");
+        }
+        String message = "*";
+        3
+
+    }
+
+}
